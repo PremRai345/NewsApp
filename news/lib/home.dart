05c66c9df3a3d4d394e02fae0510b10afa781c46
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-06-26 20:14:32 +0545

    Card is Added.
    Card widget is added where user can see news headline and short descriptions.

--- news/lib/home.dart
+++ news/lib/home.dart
@@ -1,6 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_controller.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -26,108 +25,175 @@ class _HomeState extends State<Home> {
         title: const Text('News'),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 9),
-            margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.circular(24)),
-            child: Row(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    if ((searchController.text).replaceAll(" ", "") == "") {
-                      print("Blank search");
-                    } else {
-                      // Navigator.push(context, MaterialPageRoute(builder: (context) => Search(searchController.text)));
-                    }
-                  },
-                  child: Container(
-                    child: Icon(
-                      Icons.search,
-                      color: Colors.blueAccent,
-                    ),
-                    margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
-                  ),
-                ),
-                Expanded(
-                  child: TextField(
-                    controller: searchController,
-                    textInputAction: TextInputAction.search,
-                    onSubmitted: (value) {
-                      print(value);
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 9),
+              margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(24)),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      if ((searchController.text).replaceAll(" ", "") == "") {
+                        print("Blank search");
+                      } else {
+                        // Navigator.push(context, MaterialPageRoute(builder: (context) => Search(searchController.text)));
+                      }
                     },
-                    decoration: InputDecoration(
-                        border: InputBorder.none, hintText: "Search"),
-                  ),
-                )
-              ],
-            ),
-          ),
-          Container(
-            height: 50,
-            child: ListView.builder(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemCount: navBarItem.length,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    print(navBarItem[index]);
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 10),
-                    margin: const EdgeInsets.symmetric(horizontal: 5),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(24),
-                    ),
-                    child: Center(
-                      child: Text(navBarItem[index],
-                          style: const TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                              color: Color.fromARGB(255, 146, 142, 142))),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-          CarouselSlider(
-            options: CarouselOptions(
-              height: 200,
-              autoPlay: true,
-              autoPlayInterval: const Duration(seconds: 3),
-              autoPlayAnimationDuration: const Duration(milliseconds: 800),
-              autoPlayCurve: Curves.fastOutSlowIn,
-              enlargeCenterPage: true,
-              scrollDirection: Axis.horizontal,
-              enableInfiniteScroll: false,
-            ),
-            items: items.map(
-              (item) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return InkWell(
-                      onTap: (){
-                        print("Tapped");
-                      
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 5.0, vertical: 14.0),
-                        child: Text(item),
+                    child: Container(
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.blueAccent,
                       ),
-                    );
-                  },
-                );
-              },
-            ).toList(),
-          ),
-        ],
+                      margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: searchController,
+                      textInputAction: TextInputAction.search,
+                      onSubmitted: (value) {
+                        print(value);
+                      },
+                      decoration: InputDecoration(
+                          border: InputBorder.none, hintText: "Search"),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              height: 50,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: navBarItem.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      print(navBarItem[index]);
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      margin: const EdgeInsets.symmetric(horizontal: 5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      child: Center(
+                        child: Text(navBarItem[index],
+                            style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 146, 142, 142))),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 200,
+                autoPlay: true,
+                autoPlayInterval: const Duration(seconds: 3),
+                autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enlargeCenterPage: true,
+                scrollDirection: Axis.horizontal,
+                enableInfiniteScroll: false,
+              ),
+              items: items.map(
+                (item) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return InkWell(
+                        onTap: () {
+                          print("Tapped");
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 5.0, vertical: 14.0),
+                          child: Text(item),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ).toList(),
+            ),
+            Container(
+              child: ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      elevation: 1.0,
+                      child: Stack(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            child: Image.asset("assets/images/prem.jpg"),
+                          ),
+                          Positioned(
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                            child: Container(
+                              height: 60,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Colors.black12.withOpacity(0.0),
+                                    Colors.black,
+                                  ],
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                ),
+                              ),
+                              padding: const EdgeInsets.fromLTRB(15, 15, 10, 8),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: const [
+                                  Text(
+                                    'News Headline',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    'This is the news description',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
