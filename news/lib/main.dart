22939d29b6f440d4c3ff621722264310697f334e
Prem Kumar Rai <69492512+PremRai345@users.noreply.github.com>
2022-07-02 14:48:08 +0545

    Screen Rotation is disable.
    The landscape mode in app is disable.

--- news/lib/main.dart
+++ news/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:news/home.dart';
 
 void main() {
@@ -11,6 +12,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //Disable the screen rotation
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
